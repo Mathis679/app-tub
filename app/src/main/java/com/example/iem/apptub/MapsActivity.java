02c545467fd8bb127847083c3333a4f432067930
PHAN DOMINIQUE p1402841 <dominique.phan@etu.univ-lyon1.fr>
2016-10-18 14:48:54 +0200

    SA MARCHE XD

--- app/src/main/java/com/example/iem/apptub/MapsActivity.java
+++ app/src/main/java/com/example/iem/apptub/MapsActivity.java
@@ -13,7 +13,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    public int test6;
+    public int test7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
