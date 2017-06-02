b36b8a11168714045fd8c3d85bf50b4dcd041a61
LAURENT MATHIS p1406269 <mathis.laurent@etu.univ-lyon1.fr>
2017-06-02 09:40:40 +0200

    route func

---
+++ app/src/main/java/com/example/iem/apptub/activities/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.example.iem.apptub.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.iem.apptub.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
